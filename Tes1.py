30bf5da655b40ac32b5f6ada200fe9bafdb59824
Indira <indira6166@gmail.com>
2022-02-05 22:21:34 +0700

    Latihan Python

---
+++ Tes1.py
@@ -0,0 +1,11 @@
+l = input(int())
+himp1 = list()
+
+
+def bacaMasukan(set,l):
+    a = int
+    input(a)
+    for i in range(int(l)):
+        x = input()
+        himp1.append(int(x))
+    print ('Array: ',himp1)
