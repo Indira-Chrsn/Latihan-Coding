bc63ed84667ee5e30049bcb32c816b0f630d687c
Indira <indira6166@gmail.com>
2022-02-05 23:30:45 +0700

    Latihan Go

---
+++ tes lagi.py
@@ -0,0 +1,15 @@
+#tinggi=4
+#lebar=3
+#print(tinggi*lebar)
+
+
+panjang=4
+lebar=3
+#luas = 0
+def luas_persegi_panjang(panjang, lebar):
+    luas = panjang * lebar
+    print ("Luas Persegi Panjang= ", luas)
+
+luas_persegi_panjang(6, 3)
+
+
---
+++ tes1.go
@@ -0,0 +1,79 @@
+package main
+import "fmt"
+
+func main(){	
+	var n,a,x int
+	var data [100]int
+	var min1,min2 int
+	var max1,max2 int
+	
+	fmt.Scanln(&n,&a)
+	
+	for i:=0; i < n; i++{
+		fmt.Scan(&x)
+		data[i] = x
+	}
+	
+	min1,min2 = min(data,n)
+	max1,max2 = max(data,n)
+	
+	if ganjilGenap(a) == "genap"{
+		fmt.Println(min2,min1)
+	}else{
+		fmt.Println(max1,max2)
+	}
+}
+
+func ganjilGenap(a int)string{
+	if a%2 == 0{
+		return "genap"
+	}else{
+		return "ganjil"
+	}
+}
+
+func min(data [100]int,n int)(int,int){
+	min1 := data[0]
+	min2 := data[0]
+	
+	for i:=0; i < n; i++{
+		if min1 > data[i]{
+			min1 = data[i]
+		}
+	}
+		
+	for i:=0; i < n; i++{
+		if min2 > data[i]{
+			if data[i] == min1{
+				min2 = min2
+			}else{
+				min2 = data[i]
+			}
+		}
+		
+	}
+	return min2,min1
+}
+
+func max(data [100]int,n int)(int,int){
+	max1 := data[0]
+	max2 := data[0]
+	
+	for i:=0; i < n; i++{
+		if max1 < data[i]{
+			max1 = data[i]
+		}
+	}
+		
+	for i:=0; i < n; i++{
+		if max2 < data[i]{
+			if data[i] == max1{
+				max2 = max2
+			}else{
+				max2 = data[i]
+			}
+		}
+	}
+	return max1,max2
+}
+		
